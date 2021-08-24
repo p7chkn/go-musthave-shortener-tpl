64f6f6fdf3ecca9e4977838bacdd063c017f461a
p7chkn <p7chkn@yandex.ru>
2021-08-24 20:59:03 +0300

    corrections by mentor

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,41 +3,44 @@ package handlers
 import (
 	"io/ioutil"
 	"net/http"
-	"net/url"
 
 	"github.com/gin-gonic/gin"
-	"github.com/p7chkn/go-musthave-shortener-tpl/internal/shortener"
+	"github.com/p7chkn/go-musthave-shortener-tpl/internal/models"
 )
 
-func RetriveShortURL(data url.Values) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		result := map[string]string{}
-		long, err := shortener.GetURL(c.Param("id"), data)
-
-		if err != nil {
-			result["detail"] = err.Error()
-			c.IndentedJSON(http.StatusNotFound, result)
-			return
-		}
-
-		c.Header("Location", long)
-		c.String(http.StatusTemporaryRedirect, "")
-	}
+type Handler struct {
+	repo models.RepositoryInterface
 }
 
-func CreateShortURL(data url.Values) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		result := map[string]string{}
-		defer c.Request.Body.Close()
+func (h *Handler) SetupRepository(repo models.RepositoryInterface) {
+	h.repo = repo
+}
 
-		body, err := ioutil.ReadAll(c.Request.Body)
+func (h *Handler) RetriveShortURL(c *gin.Context) {
+	result := map[string]string{}
+	long, err := h.repo.GetUrl(c.Param("id"))
 
-		if err != nil {
-			result["detail"] = "Bad request"
-			c.IndentedJSON(http.StatusBadRequest, result)
-			return
-		}
-		short := shortener.AddURL(string(body), data)
-		c.String(http.StatusCreated, "http://localhost:8080/"+short)
+	if err != nil {
+		result["detail"] = err.Error()
+		c.IndentedJSON(http.StatusNotFound, result)
+		return
 	}
+
+	c.Header("Location", long)
+	c.String(http.StatusTemporaryRedirect, "")
+}
+
+func (h *Handler) CreateShortURL(c *gin.Context) {
+	result := map[string]string{}
+	defer c.Request.Body.Close()
+
+	body, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		result["detail"] = "Bad request"
+		c.IndentedJSON(http.StatusBadRequest, result)
+		return
+	}
+	short := h.repo.AddUrl(string(body))
+	c.String(http.StatusCreated, "http://localhost:8080/"+short)
 }
